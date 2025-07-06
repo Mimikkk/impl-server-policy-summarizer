89e43db817e6aecc1ce5fee605f978d8746b0cbe
Mimikkk <zdancewicz.daniel@gmail.com>
2025-07-06 12:20:03 +0200

    not found component

--- workspace/apps/ui/src/routes/__root.tsx
+++ workspace/apps/ui/src/routes/__root.tsx
@@ -1,7 +1,11 @@
 import { ThemeButton } from "@components/actions/ThemeButton.tsx";
+import { Icon } from "@components/badges/Icon.tsx";
+import { Card } from "@components/containers/card/Card.tsx";
+import { Text } from "@components/typography/Text.tsx";
 import { DevTools } from "@features/dx/dev-tools/DevTools.tsx";
 import { useTheme } from "@features/ux/theme/ThemeProvider.tsx";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { CatIcon } from "lucide-react";
 import { useEffect } from "react";
 
 export const Route = createRootRouteWithContext()({
@@ -25,4 +29,15 @@ export const Route = createRootRouteWithContext()({
       </div>
     );
   },
+  notFoundComponent: () => (
+    <div className="container mx-auto py-8">
+      <Card className="w-full flex flex-col items-center" color="info">
+        <Text className="flex items-center text-2xl gap-1">
+          <Icon icon={CatIcon} size="lg" />
+          404
+        </Text>
+        <Text light>Page not found</Text>
+      </Card>
+    </div>
+  ),
 });
