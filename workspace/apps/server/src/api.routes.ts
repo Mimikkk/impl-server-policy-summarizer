ed82481d48bdc686329a7e4d1ca8ad9acf369a94
Mimikkk <zdancewicz.daniel@gmail.com>
2025-10-29 21:29:33 +0100

    rename

--- workspace/apps/server/src/api.routes.ts
+++ workspace/apps/server/src/api.routes.ts
@@ -1,8 +1,8 @@
 import "./core/messages/responses.ts";
 
-import "@features/csvs/csv.routes.ts";
+import "@features/csv-operations/csv.routes.ts";
 import "@features/docs/docs.routes.ts";
 import "@features/eli/eli.routes.ts";
 import "@features/metrics/metrics.routes.ts";
-import "@features/pdfs/pdfs.routes.ts";
+import "@features/pdfs-operations/pdfs.routes.ts";
 import "@features/translations/translation.routes.ts";
